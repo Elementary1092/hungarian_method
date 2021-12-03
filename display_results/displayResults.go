aa5ae60ad42b9e624392a548529b1190b3baf0b7
Elementary1092 <pdead6223@gmail.com>
2021-12-03 12:38:43 +0500

    Add total cost

--- display_results/displayResults.go
+++ display_results/displayResults.go
@@ -8,7 +8,9 @@ import (
 )
 
 func Display(t *alg.Table, coords []*c.Coord) {
+	var sum int64
 	for _, coord := range coords {
+		sum += t.ValueAt(coord.Row(), coord.Col())
 		fmt.Printf(
 			"Job %d should be done by %d worker and it will cost %d\n",
 			coord.Row() + 1,
@@ -16,4 +18,5 @@ func Display(t *alg.Table, coords []*c.Coord) {
 			t.ValueAt(coord.Row(), coord.Col()),
 		)
 	}
+	fmt.Printf("Total cost: %d", sum)
 }
